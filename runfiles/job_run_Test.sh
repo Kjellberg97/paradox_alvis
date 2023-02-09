d651bf2c85f9847134b15cfa0b5293229c97e22b
kjellberg97 <viktor.kjellberg@hotmail.se>
2023-02-09 15:56:23 +0100

    test_file added

--- runfiles/job_run_Test.sh
+++ runfiles/job_run_Test.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -A SNIC2022-22-744      # find your project with the "projinfo" command
 #SBATCH -p alvis               # what partition to use (usually not necessary)
-#SBATCH -t 0-00:10:00          # how long time it will take to run
+#SBATCH -t 0-03:00:00          # how long time it will take to run
 #SBATCH --gpus-per-node=A100:4  # choosing no. GPUs and their type
 #SBATCH -J test             # the jobname (not necessary)
 
@@ -13,4 +13,4 @@ module purge
 
 CONTAINER=~/container/paradox.sif
 
-apptainer exec $CONTAINER python ~/paradox_alvis/train_BERT_test.py
+apptainer exec $CONTAINER python ~/paradox_alvis/test_BERT.py
---
+++ test_BERT.py
@@ -0,0 +1,51 @@
+import os
+
+#LP
+os.system("bash scripts/6_eval_bert.bash 0 \
+    --val_file_path /mimer/NOBACKUP/groups/snic2022-22-744/DATA/LP/prop_examples_all.txt_test \
+    --custom_weight /mimer/NOBACKUP/groups/snic2022-22-744/OUTPUT/LP/BERT/checkpoint-19/pytorch_model.bin \
+cat eval_result.txt")
+
+os.system("bash scripts/6_eval_bert.bash 0 \
+    --val_file_path /mimer/NOBACKUP/groups/snic2022-22-744/DATA/RP/prop_examples_all.txt_test \
+    --custom_weight /mimer/NOBACKUP/groups/snic2022-22-744/OUTPUT/LP/BERT/checkpoint-19/pytorch_model.bin \
+cat eval_result.txt")
+
+os.system("bash scripts/6_eval_bert.bash 0 \
+    --val_file_path /mimer/NOBACKUP/groups/snic2022-22-744/DATA/RP_10X/prop_examples_all.txt_test \
+    --custom_weight /mimer/NOBACKUP/groups/snic2022-22-744/OUTPUT/LP/BERT/checkpoint-19/pytorch_model.bin \
+cat eval_result.txt")
+
+
+#RP
+os.system("bash scripts/6_eval_bert.bash 0 \
+    --val_file_path /mimer/NOBACKUP/groups/snic2022-22-744/DATA/LP/prop_examples_all.txt_test \
+    --custom_weight /mimer/NOBACKUP/groups/snic2022-22-744/OUTPUT/RP/BERT/checkpoint-19/pytorch_model.bin \
+cat eval_result.txt")
+
+os.system("bash scripts/6_eval_bert.bash 0 \
+    --val_file_path /mimer/NOBACKUP/groups/snic2022-22-744/DATA/RP/prop_examples_all.txt_test \
+    --custom_weight /mimer/NOBACKUP/groups/snic2022-22-744/OUTPUT/RP/BERT/checkpoint-19/pytorch_model.bin \
+cat eval_result.txt")
+
+os.system("bash scripts/6_eval_bert.bash 0 \
+    --val_file_path /mimer/NOBACKUP/groups/snic2022-22-744/DATA/RP_10X/prop_examples_all.txt_test \
+    --custom_weight /mimer/NOBACKUP/groups/snic2022-22-744/OUTPUT/RP/BERT/checkpoint-19/pytorch_model.bin \
+cat eval_result.txt")
+
+
+#RP
+os.system("bash scripts/6_eval_bert.bash 0 \
+    --val_file_path /mimer/NOBACKUP/groups/snic2022-22-744/DATA/LP/prop_examples_all.txt_test \
+    --custom_weight /mimer/NOBACKUP/groups/snic2022-22-744/OUTPUT/PR_10X/BERT/checkpoint-19/pytorch_model.bin \
+cat eval_result.txt")
+
+os.system("bash scripts/6_eval_bert.bash 0 \
+    --val_file_path /mimer/NOBACKUP/groups/snic2022-22-744/DATA/RP/prop_examples_all.txt_test \
+    --custom_weight /mimer/NOBACKUP/groups/snic2022-22-744/OUTPUT/PR_10X/BERT/checkpoint-19/pytorch_model.bin \
+cat eval_result.txt")
+
+os.system("bash scripts/6_eval_bert.bash 0 \
+    --val_file_path /mimer/NOBACKUP/groups/snic2022-22-744/DATA/RP_10X/prop_examples_all.txt_test \
+    --custom_weight /mimer/NOBACKUP/groups/snic2022-22-744/OUTPUT/PR_10X/BERT/checkpoint-19/pytorch_model.bin \
+cat eval_result.txt")
