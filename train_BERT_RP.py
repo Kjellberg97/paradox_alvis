fa420a8e99990b0e0384eeecb255334fe35d6a18
kjellberg97 <viktor.kjellberg@hotmail.se>
2023-02-24 09:38:54 +0100

    train_BERT

--- train_BERT_RP.py
+++ train_BERT_RP.py
@@ -3,7 +3,7 @@ import os
 
 os.system("bash scripts/5_train_bert.bash \
 0,1,2,3 4 8064 \
- /mimer/NOBACKUP/groups/snic2022-22-744/OUTPUT/RP/BERT/ \
+ /mimer/NOBACKUP/groups/snic2022-22-744/OUTPUT/RP/BERT2/ \
  --num_train_epochs 20.0 \
  --gradient_accumulation_steps 8 --per_gpu_train_batch_size=2 \
  --per_gpu_train_batch_size 8\
--- train_BERT_RP10X.py
+++ train_BERT_RP10X.py
@@ -4,7 +4,7 @@ import os
 # For RP_10X
 os.system("bash scripts/5_train_bert.bash \
 0,1,2,3 4 6006 \
- /mimer/NOBACKUP/groups/snic2022-22-744/OUTPUT/PR_10X/BERT/ \
+ /mimer/NOBACKUP/groups/snic2022-22-744/OUTPUT/PR_10X/BERT2/ \
  --num_train_epochs 20.0 \
  --gradient_accumulation_steps 8 --per_gpu_train_batch_size=2 \
  --train_file_path /mimer/NOBACKUP/groups/snic2022-22-744/DATA/RP_10X/prop_examples_all.txt.balanced_rulenum_train \
