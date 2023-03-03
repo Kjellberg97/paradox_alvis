9241e5b173d7003c6a2d1b83c50e1230d47903c4
Daniel Enström <denstrm@gmail.com>
2023-03-03 13:59:00 +0100

    Fixed output function in generate class.

---
+++ runfiles/job_test_reformat.sh
@@ -0,0 +1,15 @@
+#!/bin/env bash
+
+#SBATCH -A SNIC2022-22-744      # find your project with the "projinfo" command
+#SBATCH -t 0-09:00:00          # how long time it will take to run
+#SBATCH -C NOGPU  # choosing no. GPUs and their type
+#SBATCH -J create_labels             # the jobname (not necessary)
+
+# Load PyTorch using the module tree
+module purge
+
+# Load container 
+
+CONTAINER=~/container/paradox.sif
+
+apptainer exec $CONTAINER python ~/paradox_alvis/code/evaluate/reformat.py
