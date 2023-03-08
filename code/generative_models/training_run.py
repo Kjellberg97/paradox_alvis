b5216c4b4c9e7aad9c3879e2c2228b016f2e8f98
kjellberg97 <viktor.kjellberg@hotmail.se>
2023-03-08 08:34:45 +0100

    Some smaller changes in training
    Co-authored-by: denstrm <denstrm@users.noreply.github.com>

--- code/generative_models/training_run.py
+++ code/generative_models/training_run.py
@@ -1,7 +1,7 @@
 from finetune_BART import ProofGenerationModel
 
 model_path = "/mimer/NOBACKUP/groups/snic2022-22-744/MODELS/LP/"
-model_name = "pretrained_BART/OUTPUT/checkpoint-28000"
+model_name = "pretrained_BART"
 data_path = "/mimer/NOBACKUP/groups/snic2022-22-744/DATA/LP/prop_examples_all"
 
 PGM = ProofGenerationModel(model_path, model_name)
