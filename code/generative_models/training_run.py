b2b5a3054165443c307538e641e0c30c1d3150e0
Daniel Enström <denstrm@gmail.com>
2023-03-13 11:37:59 +0100

    Reformat input scripts

--- code/generative_models/training_run.py
+++ code/generative_models/training_run.py
@@ -2,8 +2,8 @@ from finetune_BART import ProofGenerationModel
 
 model_path = "/mimer/NOBACKUP/groups/snic2022-22-744/MODELS/LP/"
 model_name = "pretrained_BART"
-checkpoint = "checkpoint-22"
-data_path = "/mimer/NOBACKUP/groups/snic2022-22-744/DATA/EXAMPLE/small"
+checkpoint = None
+data_path = "/mimer/NOBACKUP/groups/snic2022-22-744/DATA/LP/prop_examples_all"
 
 PGM = ProofGenerationModel(model_path, model_name, checkpoint)
 ds = PGM.load_all_data(data_path)
