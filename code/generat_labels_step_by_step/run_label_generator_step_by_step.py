4718a757653922a280ac4ea9abd96923d385ce32
kjellberg97 <viktor.kjellberg@hotmail.se>
2023-03-22 16:08:36 +0100

    Label generation step by step
    Co-authored-by: denstrm <denstrm@users.noreply.github.com>

---
+++ code/generat_labels_step_by_step/run_label_generator_step_by_step.py
@@ -0,0 +1,57 @@
+from label_generator_step_by_step import Find_next_rule
+import csv 
+import json
+import os
+from tqdm import tqdm
+
+def read_file_lines(path):
+    dictionaries=[]
+    with open(path) as f:
+        dictionaries = json.load(f)
+    
+    return dictionaries
+
+def save_data(data, save_file_name, len_data):
+    
+    FNR = Find_next_rule(data)
+    steps = FNR.find_steps()
+
+    with open(save_file_name, 'a') as file:
+        json.dump(path, file)
+
+        if not len_data ==0:
+            file.write(",\n")
+
+
+
+def main(home_path, dic_name, file_name):
+
+    save_name = file_name + "step_labels"
+    
+    file_path = home_path + "/" + dic_name + "/" + file_name + ".txt"
+    save_path = home_path + "/" + dic_name + "/" + save_name + ".txt"
+    data = read_file_lines(file_path)
+    
+    len_data = len(data)
+
+    open(save_path, 'w').close()
+    with open(save_path, 'a') as file:
+        file.write("[")
+    for example in tqdm(data):
+        len_data -=1
+        save_data(example,save_path, len_data)
+    with open(save_path, 'a') as file:
+        file.write("]")
+
+
+
+if __name__ == "__main__":
+
+    # Define the path to the data
+    path = "/mimer/NOBACKUP/groups/snic2022-22-744/DATA"
+
+    paths_to_train = [["EXAMPLE", "small_test"]]
+
+    for p in paths_to_train:
+        
+        main(path,p[0],p[1])
