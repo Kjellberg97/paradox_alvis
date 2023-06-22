3a7c36653d6c87f6060d9c00fc5a82cc83636676
kjellberg97 <viktor.kjellberg@hotmail.se>
2023-06-22 09:56:40 +0200

    coding of words to letters and nouns
    Co-authored-by: denstrm <denstrm@users.noreply.github.com>

--- code/word_coding/coding_of_words.py
+++ code/word_coding/coding_of_words.py
@@ -15,32 +15,24 @@ def read_file_lines( file_path):
         return json.load(f)
 
 
-def save_coding(data, save_file_name= "coded_vocab.txt"):
+def save_coding(data, save_file_name= "code/word_coding/coded_vocab.txt"):
     with open(save_file_name, 'w') as file:
         json.dump(data, file)
 
 
 def coding():
-
     chars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     for c in chars:
         for n in chars:
             yield c+str(n)
 
 
-def main():
-    words = read_words("vocab.txt")
-    words = [ w.replace("\n", "") for w in words]
+def coding_nouns():
+    nouns = read_words("code/word_coding/vocab_nouns.txt")
+    nouns = [ w.replace("\n", "") for w in words]
 
-    coded_gen=coding()
-
-    coded_dict = {}
-    for w in words:
-        coded_dict[w] = next(coded_gen)
-      
-    save_coding(coded_dict)
-
-    coding_of_input(coded_dict, "/Users/vikto/OneDrive/Dokument/kurser/MASTERTHESIS/Data/DATA/LP/prop_examples_all_cleaned_test.txt")
+    for c in nouns:
+        yield c
 
 
 def replace_all(text, dic):
@@ -52,15 +44,35 @@ def replace_all(text, dic):
 def coding_of_input(coded_dict, input_file):
     data = read_file_lines(input_file)
     all_coded_input =[]
+    new_data=[]
 
     for d in data:
         input_text = d["input"]
         coded_input = replace_all(input_text, coded_dict)
         
-        all_coded_input.append(coded_input)
+        d["input"] = coded_input
+        #all_coded_input.append(coded_input)
+        new_data.append(d)
         
-    save_coding(all_coded_input, save_file_name= "coded_prop_examples_all_cleaned_test.txt")
+    save_coding(new_data, save_file_name= "/mimer/NOBACKUP/groups/snic2022-22-744/DATA/RP_10X/coded_prop_examples_all_balanced_rulenum_cleaned_test.txt")
 
-        
+
+
+def main():
+
+    words = read_words("code/word_coding/vocab.txt")
+    words = [ w.replace("\n", "") for w in words]
+    coded_gen=coding_nouns()
+
+    coded_dict = {}
+    for w in words:
+        coded_dict[w] = next(coded_gen)
+      
+    save_coding(coded_dict)
+
+    coded_dict = read_file_lines("/cephyr/users/viktorkj/Alvis/paradox_alvis/code/word_coding/coded_vocab.txt")
+    coding_of_input(coded_dict, "/mimer/NOBACKUP/groups/snic2022-22-744/DATA/RP_10X/prop_examples_all_balanced_rulenum_cleaned_test.txt")
+
+
 if __name__ == "__main__":
     main()
--- runfiles/eval_step_by_step.sh
+++ runfiles/eval_step_by_step.sh
@@ -2,9 +2,9 @@
 
 #SBATCH -A SNIC2022-22-744      # find your project with the "projinfo" command
 #SBATCH -p alvis               # what partition to use (usually not necessary)
-#SBATCH -t 0-10:00:00          # how long time it will take to run
-#SBATCH --gpus-per-node=A100:1  # choosing no. GPUs and their type
-#SBATCH -J eval_gen             # the jobname (not necessary)
+#SBATCH -t 0-20:00:00          # how long time it will take to run
+#SBATCH --gpus-per-node=A100fat:1  # choosing no. GPUs and their type
+#SBATCH -J RP_10X_ALL_test           # the jobname (not necessary)
 
 # Load PyTorch using the module tree
 module purge
